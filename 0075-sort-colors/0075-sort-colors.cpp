87d33ec4d58ffa3b05b4c7deea0fc1e8623078fc
Aditya Patel <patel.aditya2014@gmail.com>
2023-05-21 13:31:56 +0530

    Time: 3 ms (51.02%), Space: 8.2 MB (61.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // dutch national flag problem 
-        // where we need to sort three things think abou this problem 
+        // step 1: setup pointers
         int n = nums.size();
         int low = 0, mid = 0, high = n - 1;
         
+        // step 2: traver the array 
         while(mid <= high) {
             if(nums[mid] == 0) {
                 swap(nums[low], nums[mid]);
@@ -20,6 +20,9 @@ public:
         }
     }
 };
+
+// Dutch national flag problem -> think of it where ever we need to sort three things
+// The array is traversed only one time
 
 // tc -> O(n)
 // sc -> O(1)
