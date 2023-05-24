c7223f9e4846ce61562a9e46b643f338fa18c051
Aditya Patel <patel.aditya2014@gmail.com>
2023-05-24 22:37:49 +0530

    Time: 6 ms (23.79%), Space: 8.3 MB (20.65%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // store the occurence of red, white and blue objects 
-        int r = 0, w = 0, b = 0;
+        // step 1: counting the occurence of 0, 1, and 2
+        int a = 0, b = 0, c = 0;
         int n = nums.size();
-        // int count[3] = {0};
         
         for(int i = 0; i < n; i++) {
-            if(nums[i] == 0) r++;
-            else if(nums[i] == 1) w++;
-            else b++;
+            if(nums[i] == 0) a++;
+            else if(nums[i] == 1) b++;
+            else c++;
         }
         
-        // populate the input array it self 
+        // step 2: populating the given input array 
         for(int i = 0; i < n; i++) {
-            if(r > 0) {
+            if(a > 0) {
                 nums[i] = 0;
-                r--;
-            } else if(w > 0) {
+                a--;
+            } else if (b > 0) {
                 nums[i] = 1;
-                w--;
-            } else if(b > 0) {
-                nums[i] = 2;
                 b--;
+            } else {
+                nums[i] = 2;
             }
         }
         
         return;
     }
 };
-
-// tc => O(n) + O(n)
-// sc -> O(1)
