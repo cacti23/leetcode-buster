82dd4dd1d3608a1d5a99bcebdd6afd2171d3d414
Aditya Patel <patel.aditya2014@gmail.com>
2023-05-31 14:02:21 +0530

    Time: 3 ms (42.83%), Space: 7.1 MB (62.36%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
+    void reverse(vector<int>& arr, int start, int end) {
+        while(start < end) {
+            swap(arr[start++], arr[end--]);
+        }
+    }
+    
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         
-        // step 1: create a new matrix with n x n dimensions
-        vector<vector<int>> newMatrix(n, vector<int>(n, 0));
-        
-        // step 2: copy the value from given matrix to new matrix 
-        for(int row = 0; row < n; row++) {
-            for(int col = 0; col < n; col++) {
-                newMatrix[row][col] = matrix[row][col];
+        // step 1: transpose the matrix
+        for(int i = 0; i < n; i++) {
+            // j < i is really important here in order to transpose correctly 
+            // in 1st row we want only one swap ie 1, 0
+            // in 2nd row we want two swaps 2, 0 and 2, 1
+            for(int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
         
-        // step 3: copy 1st row of new of new matrix into last column to given matrix and so on
+        // step 2: reverse every row 
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                matrix[j][n - (i + 1)] = newMatrix[i][j];
-            }
+            reverse(matrix[i], 0, n - 1);
         }
         
         return;
