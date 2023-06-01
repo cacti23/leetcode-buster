9a3aa54e4574837171dd0f0e9a2762c1edd6f5e3
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-01 23:21:32 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -7,30 +7,66 @@ using namespace std;
 class Solution{
 public:
     vector<int> findTwoElement(vector<int> arr, int n) {
-        // frequency array solution 
-        vector<int> freq(n + 1, 0);
+        int xr = 0; // 0 because its kind like identity thing 
+        // even number : 2 ^ 2 = 0
+        // odd Number : 2 ^ 2 ^ 2 = 2
         
-        vector<int> ans(2, 0);
-        
-        // populate frequncey array 
+        // find xor for elements of the array and element from 1 to n
         for(int i = 0; i < n; i++) {
-            freq[arr[i]]++;
+            xr = xr ^ arr[i];
+            xr = xr ^ (i + 1);
         }
         
-        // traverse frequncy array 
-        for(int i = 1; i <= n; i++) {
-            if(freq[i] > 1) {
-                ans[0] = i;
+        // now xr = x ^ y where x is repeating and y is missing number
+        // repeating number will comes odd i.e three times when we xor all element and missing elements will comes odd i.e one 
+        
+        // now we need to find the first set bit of the xor number , we need a number who only contains 
+        // first set bit of the xr number 
+        
+        int bitIndex = 0;
+        
+        while(1) {
+            if((xr & ( 1 << bitIndex)) != 0) {
+                break;
             }
             
-            if(freq[i] == 0) {
-                ans[1] = i;
+            bitIndex++;
+        }
+        
+        int number = 1 << bitIndex;
+        
+        // now we have the index for the first bit element 
+        // now need to segrregate the elements form array and 1 to n into group whos this bit index is zero or set 
+        
+        int zero = 0;
+        int one = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if((arr[i] & number) != 0) {
+                one = one ^ arr[i];
+            } else {
+                zero = zero ^ arr[i];
+            }
+            
+            if(((i + 1) & number) != 0) {
+                one = one ^ (i + 1);
+            } else {
+                zero = zero ^ (i + 1);
             }
         }
         
-        return ans;
+        // after this for loop one and zero will have one of the missing or repearing number 
+        // find thos number 
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == one) cnt++;
+        }
+        
+        if(cnt == 2) return { one, zero}; // { repeating, missing }
+        return { zero, one };
     }
 };
+
 
 //{ Driver Code Starts.
 
