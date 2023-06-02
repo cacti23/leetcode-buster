a3e92451fb73e0d5140a0abaad2c1294faa8e98f
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-02 18:49:17 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (37.19%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,14 +5,43 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
         
-        // traverse the whole matrix 
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(matrix[i][j] == target) {
-                    return true;
-                }
+        if(rows == 1) {
+            for(int i = 0; i < cols; i++) {
+                if(target == matrix[0][i]) return true;
             }
         }
+        
+        int rowToSearch = -1;
+        
+        // traverse the last column
+        for(int i = 1; i < rows; i++) {
+            int ele1 = matrix[i - 1][cols - 1]; // last element of previous row
+            int ele2 = matrix[i][cols - 1]; // last element of current row
+            // we know that ele1 is less than ele2
+            /// write conditions
+            
+            if(target <= ele1) {
+                if(target == ele1) return true;
+                rowToSearch = i - 1;
+                break;
+            } else if(target > ele1 && target <= ele2) {
+                if(target == ele2) return true;
+                rowToSearch = i;
+                break;
+            } 
+        }
+        
+        
+        if(rowToSearch == -1) {
+            return false;
+        }
+        
+        
+        // we have a row where we can find the element 
+        for(int i = 0; i < cols; i++) {
+            if(target == matrix[rowToSearch][i]) return true;
+        }
+        
         
         return false;
     }
