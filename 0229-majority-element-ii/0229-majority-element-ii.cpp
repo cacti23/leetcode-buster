1755c6754492b680c846cb2ac7034a4e69f31d91
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-04 16:15:12 +0530

    Time: 23 ms (8.56%), Space: 15.8 MB (73.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,22 +3,37 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         
+        int ele1 = -1, cnt1 = 0, ele2 = -1, cnt2 = 0;
+        
+        for(int i = 0; i < n; i++) {
+            // for ele 1 and cnt 1
+            if(cnt1 == 0 && nums[i] != ele2) {
+                ele1 = nums[i];
+                cnt1++;
+            } else if(cnt2 == 0 && nums[i]!= ele1) {
+                ele2 = nums[i];
+                cnt2++;
+            } else if(nums[i] == ele1) {
+                cnt1++;
+            } else if(nums[i] == ele2) {
+                cnt2++;
+            } else {
+                cnt1--, cnt2--;
+            }
+        }
+        
         vector<int> ans;
-        
-        map<int, int> mpp;
-        
-        for(int i = 0; i < n; i++) {
-            mpp[nums[i]]++;
+        cnt1 = 0, cnt2 = 0;
+        // verfiy if the stored elements are in majority or not 
+        for(int i = 0; i < n; i++)  {
+            if(ele1 == nums[i]) cnt1++;
+            
+            else if(ele2 == nums[i]) cnt2++;
         }
         
-        for(int i = 0; i < n; i++) {
-                
-            if(ans.size() == 1 && ans[0] == nums[i]) continue;
-            
-            if(mpp[nums[i]] > n / 3) ans.push_back(nums[i]);
-            
-            if(ans.size() == 2) break;
-        }
+        if(cnt1 > n/3) ans.push_back(ele1);
+        
+        if(cnt2 > n/3) ans.push_back(ele2);
         
         return ans;
     }
