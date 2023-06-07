fe634840b9928957c989a4723d81e6ce386be499
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-07 12:59:34 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (95.22%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        // brute force
+        // tortoise hare approach
         ListNode* slow = head;
         ListNode* fast = head;
         
