bd06651a8ae70fb32241d6857036b1734b02077b
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-08 13:40:09 +0530

    Time: 47 ms (27.45%), Space: 71.6 MB (9.80%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,65 +11,32 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* curr1 = l1;
-        ListNode* curr2 = l2;
-        ListNode* ansHead = new ListNode(-1);
-        ListNode* ansCurr = ansHead;
-        
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
         int carry = 0;
         
-        while(curr1 != NULL && curr2 != NULL) {
-            int sum = curr1 -> val + curr2 -> val + carry;
+        while(l1 != NULL || l2 != NULL || carry) {
+            int sum = carry;
             
-            // sum can range from 0 -> 18
-            // check if carry is there or not 
+            if(l1 != NULL) {
+                sum += l1 -> val;
+                l1 = l1 -> next;
+            }
+            
+            if(l2 != NULL) {
+                sum += l2 -> val;
+                l2 = l2 -> next;
+            }
+            
             carry = sum / 10;
             
             sum = sum % 10;
             
-            ansCurr -> next = new ListNode(sum);
-            
-            ansCurr = ansCurr -> next;
-            
-            curr1 = curr1 -> next;
-            curr2 = curr2 -> next;
+            curr -> next = new ListNode(sum);
+            curr = curr -> next;
         }
         
-        // after the above loop either elements of l1 remains or elements of l2 remains
-        while(curr1 != NULL) {
-            int sum = curr1 -> val + carry;
-            
-            carry = sum / 10;
-            
-            sum  = sum % 10;
-            
-            ansCurr -> next = new ListNode(sum);
-            ansCurr = ansCurr -> next;
-            
-            curr1 = curr1 -> next;
-        }
-        
-        while(curr2 != NULL) {
-            int sum = curr2 -> val + carry;
-            
-            carry = sum / 10;
-            
-            sum  = sum % 10;
-            
-            ansCurr -> next = new ListNode(sum);
-            ansCurr = ansCurr -> next;
-            
-            curr2 = curr2 -> next;
-        }
-        
-        // maybe carry is still there 
-        if(carry > 0) {
-            ansCurr -> next = new ListNode(carry);
-        }
-        
-        return ansHead -> next;
-        
-        
+        return dummy -> next;
     }
 };
 
