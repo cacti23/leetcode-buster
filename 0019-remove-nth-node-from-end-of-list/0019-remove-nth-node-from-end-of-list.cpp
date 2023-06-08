32b3c67808358619376a1283e2a20b39f2f0b726
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-08 13:03:27 +0530

    Time: 7 ms (37.04%), Space: 10.6 MB (55.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -17,28 +17,38 @@ public:
         ListNode* dummy = new ListNode(-1);
         dummy -> next = head;
         
-        ListNode* curr = head;
-        int len = 0;
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
         
-        // calculate length
-        while(curr != NULL) {
-            len++;
-            curr = curr -> next;
+        // move fast n times 
+        // so totalNode - n nodes remains after fast 
+        // move both slow and fast by one and when fast is at the end 
+        // slow will be at totalNode - n
+        
+        // this loop will run n times
+         while(n > 0) {
+                fast = fast -> next;
+                n--;
+        }
+        // now fast is at n position
+        
+                
+        while(fast -> next != NULL) {      
+            slow = slow -> next;
+            fast = fast -> next;
         }
         
-        curr = dummy;
+        // now slow is at total - n node from begining 
+        ListNode* nodeToRemove = slow -> next;
+        slow -> next = nodeToRemove -> next;
         
-        // now we have to traverse till totalNode - n which is the node just before the on we want to delete 
-        for(int i = 0; i < len - n; i++) {
-            curr = curr -> next;
-        }
-        
-        // in case if len is equal to n then above for loop wont run and curr will be at dummy only
-        ListNode* nodeToRemove = curr -> next;
-        curr -> next = nodeToRemove -> next;
         delete nodeToRemove;
         
         
         return dummy -> next;
     }
 };
+
+// m is the mth node from end 
+// tc -> O(m) + O(totalNode - m) -> O(n)
+// sc -> O(1)
