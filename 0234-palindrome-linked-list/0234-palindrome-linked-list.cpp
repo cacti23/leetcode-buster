f891cba2f6bddc811bec0a176eb829b59a0d6c62
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-09 18:31:48 +0530

    Time: 280 ms (28.12%), Space: 118.1 MB (53.50%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,26 +10,74 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        // use array data structure
-        vector<int> arr;
-        
-        // copy element into array 
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
         ListNode* curr = head;
+        
         while(curr != NULL) {
-            arr.push_back(curr -> val);
-            curr = curr -> next;
+            ListNode* temp = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = temp;
         }
         
-        // now check if the array is palindrome or not 
-        int n = arr.size();
-        for(int i = 0; i < n / 2; i++) {
-            if(arr[i] != arr[n - i - 1]) return false;
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head -> next == NULL) return true;
+        // using slow and fast pointers 
+        
+        // first find the middle of linked list 
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        // now the while loop condition will be written in a way that 
+        // for even length linked list like 4 middle element index will be 1
+        // for odd length linked list like 4 middle element index will be 2
+        while(fast -> next != NULL && fast -> next -> next != NULL) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        
+        // now slow has the middle element as it would map to a 0 based index
+        // then reverse the linked list from slow -> next
+        ListNode* nodeToConnect = reverse(slow -> next);
+        // previously it was 1 -> 2 -> 3 -> 2 -> 1
+        // slow pointer is at 3 and we need to reverse 2 -> 1 which reverse function did 
+        // now need to update slow -> next to point to one
+        slow -> next = nodeToConnect;
+        
+        // now move slow to its next element 
+        slow = slow -> next;
+        
+        // so for this 1 -> 2 -> 3 -> 1 -> 2 updated linked list 
+        // set slow at 1 (2nd one) and dummy at 1 (1st one)
+        ListNode* dummy = head;
+        
+        // now use loop to check 
+        while(dummy != NULL && slow != NULL) {
+            if(dummy -> val != slow -> val) return false;
+            
+            dummy = dummy -> next;
+            slow = slow -> next;
         }
         
         return true;
     }
 };
 
-// tc -> O(n) + O(n)
-// sc -> O(n)
+// tc -> O(n/2)+O(n/2)+O(n/2)
+// sc -> O(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
