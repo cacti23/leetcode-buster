02c74df84885f8eb65031041ffb91dc960b7f4b9
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-14 16:20:35 +0530

    Time: 15 ms (9.95%), Space: 11.6 MB (68.19%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,51 +11,27 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head == NULL || head -> next == NULL) return head;
-        
-        // calculate length 
-        int len = 0;
-        
-        ListNode* curr = head;
-        ListNode* tail = NULL;
-        
-        while(curr != NULL) {
+        if(head == NULL || head->next == NULL) return head;
+
+        int len = 1;
+        ListNode* tail = head;
+        while(tail->next != NULL) {
             len++;
-            // last node
-            if(curr -> next == NULL) tail = curr;
-            
-            curr = curr -> next;
+            tail = tail->next;
         }
         
-        // if k is equal to len so there is not need to rotate list
-        if(k == len) return head;
-        
-        // if k is multiple of len then just return head
-        if(k > len && (k % len) == 0) return head;
-        
-        // if k is not the multiple of len
-        // k will be updated and that's how many rotation we need to do
-        if(k > len) k = (k % len);
-        
-        // here k will be smaller than len
-        
-        curr = head;
-        
+        k %= len;
+        if(k == 0) return head;
+
+        ListNode* newTail = head;
         for(int i = 1; i < len - k; i++) {
-            curr = curr -> next;
+            newTail = newTail->next;
         }
-        
-        // curr -> next element will be new head
-        // first connect tail with current head
-        tail -> next = head;
-        // break curr -> next 
-        ListNode* newHead = curr -> next;
-        curr -> next = NULL;
-        
-        
+
+        tail->next = head;
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+
         return newHead;
     }
 };
-
-// tc -> O(n)
-// sc -> O(1)
