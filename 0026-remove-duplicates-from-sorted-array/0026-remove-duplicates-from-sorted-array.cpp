754c970faef6dd8bf59414d40bae6634417bd487
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-20 13:47:28 +0530

    Time: 16 ms (44.60%), Space: 19 MB (12.18%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // use ordered set 
+        set <int> oset;
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            oset.insert(nums[i]);
+        }
+        
+        // here k will be the answer 
+        // which indicates the number of unique elements
+        int k = oset.size();
+        
+        // now copy unique elements to the input array
+        int i = 0;
+        for(auto it = oset.begin(); it != oset.end(); it++) {
+            nums[i++] = *it;
+        }
+        
+        return k;
+        
+    }
+};
+
+// tc -> O(n * logn)
+// sc -> O(m) , where m is the number of unique elements
