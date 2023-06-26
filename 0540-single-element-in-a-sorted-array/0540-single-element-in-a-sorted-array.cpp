44dd5b7fa7ca7b502c03f230353a255bc8058b80
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-26 19:56:13 +0530

    Time: 32 ms (44.44%), Space: 22.3 MB (35.21%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // using xor operator
+        // using binary search 
         int n = nums.size();
         
-        int ele = 0;
+        if(n == 1) return nums[0];
         
-        for(int i = 0; i < n; i++) {
-            ele = ele ^ nums[i];
+        // n will always be odd
+        
+        int low = 0, high = n - 1;
+        
+        // keping single element as reference 
+        // left half -> 1st instance -> even Index and 2nd instance -> odd index
+        // right half -> 1st instance -> Odd Index and 2nd instance -> even index
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            // check if mid is even
+            if(mid % 2 == 0) {
+                // determine which half we are present in
+                if(nums[mid] != nums[mid + 1]) {
+                    // second instance 
+                    high = mid - 1;
+                } else {
+                    // first instance 
+                    low = mid + 1;
+                }
+            } else {
+                // mid is odd
+                if(nums[mid] != nums[mid + 1]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+                
+            }
         }
         
-        return ele;
+        return nums[low];
     }
 };
-
-// tc -> O(n)
-// sc -> O(1)
