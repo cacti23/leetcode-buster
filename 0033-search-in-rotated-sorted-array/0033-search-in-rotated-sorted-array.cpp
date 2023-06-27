d0c42e4e348499e6deb0ac70243f4fd1e0cff470
Aditya Patel <patel.aditya2014@gmail.com>
2023-06-27 10:37:46 +0530

    Time: 3 ms (79.34%), Space: 11 MB (51.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,13 +1,42 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // use for loop 
-        
+        // use binary serach 
         int n = nums.size();
         
-        for(int i = 0; i < n; i++) {
-            if(target == nums[i]) return i;
+        int low = 0, high = n - 1;
+        
+        // obervation make is either left half is sorted or right half is sorted 
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if(nums[mid] == target) {
+                return mid;
+            }
+            // if left half is sorted 
+            if(nums[low] <= nums[mid]) {
+                // if elements exists between low and mid
+                if(nums[low] <= target && nums[mid] >= target) {
+                    high = mid - 1;
+                } else {
+                    // if the element does not exists in this half
+                    low = mid + 1;
+                }
+            } else {
+                // right half is sorted
+                // if element exists between high and mid
+                if(nums[mid] <= target && nums[high] >= target) {
+                    low = mid + 1;
+                } else {
+                    // if the elements does not exists in this half
+                    high = mid - 1;
+                }
+            }
+           
+    
         }
+        
         
         return -1;
     }
