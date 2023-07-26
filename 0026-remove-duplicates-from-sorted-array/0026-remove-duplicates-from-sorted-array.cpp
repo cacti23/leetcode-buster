33462be9c467cfc8c8af002d21d77d411342fba6
Aditya Patel <patel.aditya2014@gmail.com>
2023-07-26 13:51:54 +0530

    Time: 8 ms (89.42%), Space: 18.3 MB (88.32%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // use hashset
-        set<int> hset;
+        // use two pointer
         int n = nums.size();
         
-        for(int i = 0; i < n; i++) {
-            hset.insert(nums[i]);
+        if(n == 1) return 1;
+        
+        int i = 0;
+        for(int j = 1; j < n; j++) {
+            // if i and j are not equal 
+            // increment i and swap with j
+            if(nums[i] != nums[j]) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
         }
         
-        int k = hset.size();
-        int j = 0;
-        
-        for(auto x: hset) {
-            nums[j++] = x;
-        }
-        
-        return k;
+        // need to return the number of unique elements hence incremented i with one
+        return i + 1;
     }
 };
 
-// tc -> O(n*logn) + O(n)
+// tc -> O(n)
 // sc -> O(n)
