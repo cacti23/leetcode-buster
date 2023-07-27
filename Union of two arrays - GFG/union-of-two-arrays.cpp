732168aa131392243d3c2810908c41857cdda6bf
Aditya Patel <patel.aditya2014@gmail.com>
2023-07-27 17:11:02 +0530

    Added solution - LeetHub

--- Union of two arrays - GFG/union-of-two-arrays.cpp
+++ Union of two arrays - GFG/union-of-two-arrays.cpp
@@ -11,26 +11,56 @@ class Solution{
     public:
     //Function to return the count of number of elements in union of two arrays.
     int doUnion(int a[], int n, int b[], int m)  {
-        // use hashMap
-        map<int, int> freq;
+        // using two pointers when the array is sorted
+        int cnt = 0;
         
-        // traverse the array a
-        for(int i = 0; i < n; i++) {
-            freq[a[i]]++;
+        sort(a, a + n);
+        sort(b, b + m);
+        
+        int i = 0, j = 0;
+        vector<int> unionArr;
+        
+        while(i < n && j < m) {
+            if(a[i] <= b[j]) {
+                if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+                    unionArr.push_back(a[i]);
+                }
+                
+                i++;
+            } else {
+                if(unionArr.size() == 0 || unionArr.back() != b[j]) {
+                    unionArr.push_back(b[j]);
+                }
+                
+                j++;
+            }
         }
         
-        // travserse the array b
-        for(int i = 0; i < m; i++) {
-            freq[b[i]]++;
+        // check the remaining element 
+        while(i < n) {
+            if(unionArr.size() == 0 || unionArr.back() != a[i]) {
+                unionArr.push_back(a[i]);
+            }
+            
+            i++;
         }
         
-        // count the elements in the union
-        return freq.size();
+        while(j < m) {
+            if(unionArr.size() == 0 || unionArr.back() != b[j]) {
+                unionArr.push_back(b[j]);
+            }
+            
+            j++;
+        }
+        
+        return unionArr.size();
     }
 };
 
-// tc -> O(nlogn) + O(mlogm)
-// sc -> O(max(n, m))
+// did not count the time complexity of sorting function
+
+// tc -> O(n + m)
+// sc -> O(1)
 
 
 //{ Driver Code Starts.
