46241121186ff676d7df28e6a717c6b691f935c8
Aditya Patel <patel.aditya2014@gmail.com>
2023-07-27 13:07:18 +0530

    Time: 41 ms (19.59%), Space: 25 MB (62.95%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
+    void reverse(vector<int> &nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            
+            start++;
+            end--;
+        }
+        
+        return;
+    }
+    
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         
-        // if k is greater than n then we have to do modulo
+        // if k is greater than n perform the modulo operation
         if(k >= n) {
             k %= n;
         }
         
-        // now k will be less than n
-        vector<int> temp(n, 0);
-        int idx = 0;
+        // now k will be always less than n
         
-        // there will be a index that divides the given array 
-        // later part will come first in the answer
-        int ind = n - k;
+        // reverse the whole array once
+        reverse(nums, 0, n - 1);
         
-        // now copy the later part into temp array
-        for(int i = ind; i < n; i++) {
-            temp[idx++] = nums[i];
-        }
+        // now we need to reverse the left and right part separately 
+        // left part 
+        reverse(nums, 0, k - 1);
         
-        // now copy the first part into temp arry 
-        for(int i = 0; i < ind; i++) {
-            temp[idx++] = nums[i];
-        }
+        // right part
+        reverse(nums, k, n - 1);
         
-        // temp array is the answer so copy it back to nums array 
-        for(int i = 0; i < n; i++) {
-            nums[i] = temp[i];
-        }
-        
-        return; 
+        return;
     }
 };
 
-// tc -> O(n) + O(n) + O(n)
-// sc -> O(n)
+// tc -> O(n)
+// sc -> O(1)
