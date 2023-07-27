216379de8014441a57f4adf0aa45395db19c8448
Aditya Patel <patel.aditya2014@gmail.com>
2023-07-27 19:47:04 +0530

    Time: 11 ms (98.59%), Space: 20 MB (18.26%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // using xor
-        int xr = 0;
+        // using hashing
+        unordered_map<int, int> umap;
+        
         int n = nums.size();
         
+        // populate the map
         for(int i = 0; i < n; i++) {
-            xr ^= nums[i];
+            umap[nums[i]]++;
         }
         
-        return xr;
+        // check which elements is only present once 
+        for(auto it: umap) {
+            if(it.second == 1) {
+                return it.first;
+            }
+        }
+        
+        return -1;
     }
 };
+
+// tc -> O(n) + O(n)
+// sc -> O(n)
