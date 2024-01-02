091712fdc81a1bc49e198ceea21b33e1e8f6e759
Aditya Patel <patel.aditya2014@gmail.com>
2024-01-02 21:09:35 +0530

    Time: 152 ms (6.78%), Space: 44.2 MB (91.36%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    // first sorting array in ascending order so that our two pointer works
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+        
+    // using two pointers
+    let i = 0, j = 0;
+    
+    while(i < g.length) {
+        // find the cookie that can satisfy the child
+        while(j < s.length && g[i] > s[j]) {
+            j++;
+        }
+        
+        // after finding the correct cookie
+        if(j < s.length) {
+            i++;
+            j++;
+        } else {
+            break;
+        }
+    }
+    
+    return i;
+};
+
+
+// tc -> O(nlogn + mlogn + n) => O(nlogn)
+// sc -> O(1)
