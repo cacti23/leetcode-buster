2952cb12cb362ff36242d55d14929bc48ed67560
Aditya Patel <patel.aditya2014@gmail.com>
2024-01-03 20:46:24 +0530

    Time: 60 ms (13.68%), Space: 42.2 MB (30.69%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -4,11 +4,32 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function(word1, word2) {
-    word1 = word1.join('');
-    word2 = word2.join('');
+    // index of words
+    let w1 = 0, w2 = 0;
+    // index of char
+    let i = 0, j = 0;
     
-    return word1 === word2;
+    while (w1 < word1.length && w2 < word2.length) {
+        // if they are not equal
+        if(word1[w1][i] != word2[w2][j]) return false;
+        
+        i++;
+        j++;
+        if(i == word1[w1].length) {
+            w1++;
+            i = 0;
+        }
+        
+        if(j == word2[w2].length) {
+            w2++;
+            j = 0;
+        }
+    }
+    
+    if(w1 != word1.length || w2 != word2.length) return false;
+    
+    return true;
 };
 
 // tc -> O(n + m)
-// sc -> O(n + m)
+// sc -> O(1)
