d0f4477d095f3d04a7f88d59c310048590a62c52
Aditya Patel <patel.aditya2014@gmail.com>
2024-01-21 15:09:13 +0530

    Time: 57 ms (65.24%), Space: 50.9 MB (5.00%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -3,34 +3,20 @@
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(fb, n) {
-    const l = fb.length;
+var canPlaceFlowers = function(flowerbed, n) {
+    const flowerbedLength = flowerbed.length;
+
+    if (n === 0) return true;
+    if (flowerbedLength === 0) return true;  // Handle empty flowerbed
     
-    if(n === 0) return true;
-    
-    if(fb.length === 1) {
-        if(fb[0] === 0) n--;
-        
-        if(n == 0) return true;
-        return false;        
-    } 
-    
-    for(let i = 0; i < l; i++) {
-        // can plant a flower 
-        if(fb[i - 1] === undefined && fb[i + 1] === 0 && fb[i] === 0) {
+    for (let i = 0; i < flowerbedLength; i++) {
+        if ((i === 0 || flowerbed[i - 1] === 0) && flowerbed[i] === 0 && (i === flowerbedLength - 1 || flowerbed[i + 1] === 0)) {
             n--;
-            fb[i] = 1;
-        } else if (fb[i - 1] === 0 && fb[i + 1] === 0 && fb[i] === 0) {
-            n--;
-            fb[i] = 1;
-        } else if(fb[i - 1] === 0 && fb[i + 1] === undefined && fb[i] === 0) {
-            n--;
-            fb[i] = 1;
+            flowerbed[i] = 1;
+            if (n === 0) return true;  // Early return
         }
-        
-        
-        if(n == 0) return true;
     }
+
     return false;
 };
 
