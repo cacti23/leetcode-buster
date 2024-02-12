ddd5faf245680d680c3a576ff253554e12832612
Aditya Patel <patel.aditya2014@gmail.com>
2024-02-12 23:24:38 +0530

    Time: 56 ms (48.39%), Space: 51.2 MB (5.03%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -3,28 +3,31 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function(arr) {
-    let freq = new Map();
+    // value of array ranges from -1000 to 1000 so we can say max 2001 elements
+    let m = new Array(2001).fill(0);
+    // array to store the frequency of frequency in the above array
+    let n = new Array(1001).fill(0);
     
-    // populate the frequency map
+    // count the occurrence of each number in the array 
     for(let num of arr) {
-        if(freq.has((num))) {
-            freq.set(num, (freq.get(num) + 1));
-        } else {
-            freq.set(num, 1);
+        m[num + 1000]++;
+    }
+    
+    // since we have the freq which cannot be greater than 1000 at this point
+    for(let i = 1; i < 2001; i++) {
+        if(m[i] > 0) {
+            let count = m[i];
+            // if the count which is frequncy of the element is greater than on means this freq is being repeated
+            if(n[count] >= 1) return false;
+            n[count]++;
         }
     }
     
-    // create a set and add map key values to it
-    const mySet = new Set();
-    for(let val of freq.values()) {
-        if(mySet.has(val)) return false;
-        mySet.add(val);
-    }
-    
     return true;
+    
 };
 
 // tip of the day: javacript object keys are always string or symbols. If you provide integers js will automatically convert it into string
 
 // tc -> O(n)
-// sc -> O(m)
+// sc -> O(1)
