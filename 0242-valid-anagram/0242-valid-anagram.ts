ccf3fe753958980016a465ed3f3f9b65f4523fda
Aditya Patel <patel.aditya2014@gmail.com>
2024-06-13 23:12:34 +0530

    Time: 91 ms (18.89%), Space: 55 MB (44.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -3,21 +3,11 @@ function isAnagram(s: string, t: string): boolean {
     let m = t.length;
     if(n !== m) return false;
     
-    const freq = new Array<number>(26).fill(0);
+    const sortS = s.split('').sort().join('');
+    const sortT = t.split('').sort().join('');
     
-    for(let i = 0; i < n; i++) {
-        freq[s.charCodeAt(i) - 97]++;
-    }
-    
-    // now the frequency array setup is done
-    for(let i = 0; i < m; i++) {
-        let ind = t.charCodeAt(i) - 97;
-        if(freq[ind] <= 0) return false;
-        freq[ind]--;
-    }
-    
-    return true;
+    return sortS === sortT;
 };
 
-// tc -> O(n)
-// sc -> O(1)
+// tc -> O(nlogn)
+// sc -> O(n)
