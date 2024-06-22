1c92d0fd5580269eafc20a7614546f892f61b977
Aditya Patel <patel.aditya2014@gmail.com>
2024-06-22 18:09:30 +0530

    Time: 63 ms (77.85%), Space: 52.7 MB (74.38%) - LeetHub

--- 0443-string-compression/0443-string-compression.ts
+++ 0443-string-compression/0443-string-compression.ts
@@ -1,23 +1,29 @@
 function compress(chars: string[]): number {
-    let compressedString = '';
+    let compressedArr: string[] = [];
     let countConsecutive = 0;
-    
-    for(let i = 0; i < chars.length; i++) {
-        countConsecutive++;
         
-        if(i + 1 >= chars.length ||chars[i] !== chars[i + 1]) {
-            if(countConsecutive > 1) compressedString += chars[i] + countConsecutive;
-            else compressedString += chars[i];
+    for(let i = 0; i < chars.length; i++) {
+        countConsecutive++;        
+        if(i + 1 >= chars.length || chars[i] !== chars[i + 1]) {
+            compressedArr.push(chars[i]);
+            
+            if(countConsecutive > 1) {
+                let countStr = countConsecutive.toString();
+                compressedArr.push(...countStr.split(''));
+            }
+            
             countConsecutive = 0;
         }
-        
+    }
+    
+    // copy back to the chars array 
+    for(let i = 0; i < compressedArr.length; i++) {
+        chars[i] = compressedArr[i];
+    }
 
-    }
-    
-    for(let i = 0; i < compressedString.length; i++) {
-        chars[i] = compressedString[i];
-    }
-    
-    return compressedString.length;
+    return compressedArr.length;
 };
 
+
+// tc -> O(p) where p is the lenght of the original string
+// sc -> O(p) worst case
