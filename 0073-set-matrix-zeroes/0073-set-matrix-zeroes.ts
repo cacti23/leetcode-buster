14c8dd78598030322b02da9c4a207e384e61900f
Aditya Patel <patel.aditya2014@gmail.com>
2024-06-22 20:10:35 +0530

    Time: 70 ms (92.64%), Space: 54.5 MB (53.32%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -4,28 +4,69 @@
 function setZeroes(matrix: number[][]): void {
     let n = matrix.length;
     let m = matrix[0].length;
-    let row = new Array(n).fill(false);
-    let col = new Array(m).fill(false);
 
-    for(let i = 0; i < n; i++) {
-        for(let j = 0; j < m; j++) {
-            if(matrix[i][j] === 0) {
-                row[i] = true;
-                col[j] = true;
-            }
+    // using 1st row and col as to store if zero exists or not
+    // 1st row will store the zeros present in cols 
+    // 1st col will store the zeros present in rows
+    let rowHasZero = false;
+    let colHasZero = false;
+
+    // check if 1st row has zero
+    // means changing value of col
+    for(let j = 0; j < m; j++) {
+        if(matrix[0][j] == 0) {
+            rowHasZero = true;
+            break;
         }
     }
 
-
-    // now update rows and cols of matrix 
+    // check if 1st col has zero
+    // means changing value of row
     for(let i = 0; i < n; i++) {
-        for(let j = 0; j < m; j++) {
-            if(row[i] == true || col[j] == true) {
+        if(matrix[i][0] == 0) {
+            colHasZero = true;
+            break;
+        }
+    }
+
+    // now for the rest of the matrix
+    for(let i = 1; i < n; i++) {
+        for(let j = 1; j < m; j++) {
+            if(matrix[i][j] == 0) {
+                // row update
+                // since col zero store row
+                matrix[i][0] = 0;
+                // col update
+                // since row zero store col
+                matrix[0][j] = 0;
+            }
+        }
+    }
+    
+    // now set zeros for the matrix
+    for(let i = 1; i < n; i++) {
+        for(let j = 1; j < m; j++) {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                 matrix[i][j] = 0;
             }
         }
     }
+    
+    // if rowHasZero is true set the first the first row as zero
+    if(rowHasZero) {
+        for(let j = 0; j < m; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    if(colHasZero) {
+        for(let i = 0; i < n; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    return;
 };
 
-// tc -> O(m x n)
-// sc -> O(m + n)
+// tc -> O(mxn)
+// sc -> O(1)
