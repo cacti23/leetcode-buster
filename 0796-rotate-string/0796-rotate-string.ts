ac6a8890499297a4060e88d0dc3efd310ffbd922
Aditya Patel <patel.aditya2014@gmail.com>
2024-06-24 23:02:01 +0530

    Time: 75 ms (6.96%), Space: 50.3 MB (59.13%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.ts
+++ 0796-rotate-string/0796-rotate-string.ts
@@ -2,29 +2,29 @@ function rotateString(s: string, goal: string): boolean {
     if(s.length !== goal.length) return false;
     
     let n = s.length;
+    let m = n * 2;
     
-    // find on string inside another
+    let con = s + s;
     
-    // outer loop for source string
-    for(let i = 0; i < n; i++) {
-        let found = true;
-        // inner loop for goal string
-        for(let j = 0; j < n; j++) {
-            if(s[(i + j) % n] !== goal[j]) {
-                found = false;
-                break;
-            }
-        }
-        
-        // after inner loop gets completed then check 
-        if(found) {
-            return true;
+    // two pointers 
+    let i = 0, j = 0;
+    
+    while(i < m) {
+        if(con[i] === goal[j]) {
+            i++;
+            j++;
+            if(j === n) return true;
+        } else {
+            if(j != 0) {
+                i = i - j + 1;
+                j = 0;
+            } // if the string are of same length but content is different "abcde" "abced" also they matched the another substring pattern not the one which is actually needed to be matched
+            else i++;
         }
     }
     
-    return false
+    return false;
 };
-// we did not double the string hence space complexity is reduced
 
-// tc -> O(n*m) which will be O(n^2)
-// sc -> O(1)
+// tc -> O(2n) -> O(n)
+// sc -> O(n)
