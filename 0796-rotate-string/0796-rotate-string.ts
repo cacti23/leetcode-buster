ba2b616a41efdaae26f610db43374b37c1fe3a1c
Aditya Patel <patel.aditya2014@gmail.com>
2024-06-24 22:36:20 +0530

    Time: 68 ms (12.17%), Space: 50.3 MB (61.74%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.ts
+++ 0796-rotate-string/0796-rotate-string.ts
@@ -1,10 +1,30 @@
 function rotateString(s: string, goal: string): boolean {
     if(s.length !== goal.length) return false;
     
-    const double = s + s;
+    let n = s.length;
     
-    return double.includes(goal);
+    // find on string inside another
+    
+    // outer loop for source string
+    for(let i = 0; i < n; i++) {
+        let found = true;
+        // inner loop for goal string
+        for(let j = 0; j < n; j++) {
+            if(s[(i + j) % n] !== goal[j]) {
+                found = false;
+                break;
+            }
+        }
+        
+        // after inner loop gets completed then check 
+        if(found) {
+            return true;
+        }
+    }
+    
+    return false
 };
+// we did not double the string hence space complexity is reduced
 
 // tc -> O(n*m) which will be O(n^2)
-// sc -> O(n)
+// sc -> O(1)
