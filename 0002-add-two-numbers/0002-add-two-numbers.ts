758d51774c4ba49676c678dd16f2a6cc8436a4a5
Aditya Patel <patel.aditya2014@gmail.com>
2024-07-13 21:14:27 +0530

    Time: 106 ms (83.14%), Space: 57.8 MB (37.38%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.ts
+++ 0002-add-two-numbers/0002-add-two-numbers.ts
@@ -15,25 +15,38 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
     let current = dummyHead;
     let carry = 0;
     
-    while(l1 !== null || l2 !== null || carry !== 0) {
+    
+    while(l1 || l2) {
         let sum = carry;
         
-        if(l1 !== null) {
+        if(l1) {
             sum += l1.val;
-            l1 = l1.next;
         }
         
-        if(l2 !== null) {
+        if(l2) {
             sum += l2.val;
-            l2 = l2.next;
         }
         
         const digit = sum % 10;
         carry = Math.floor(sum / 10);
         
-        current.next = new ListNode(digit);
+        if(l1) {
+            l1.val = digit;
+            current.next = l1;
+        } else if(l2) {
+            l2.val = digit;
+            current.next = l2;
+        } 
+        
         current = current.next;
+        l1 = l1?.next;
+        l2 = l2?.next;
+        
     }
-    
+        
+    if(carry !== 0) {
+        current.next = new ListNode(carry);
+    }
+        
     return dummyHead.next;
 };
