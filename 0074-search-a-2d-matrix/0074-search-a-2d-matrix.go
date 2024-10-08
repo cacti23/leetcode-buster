5618f90165f4d583b434e0fd8094cbf030c63b28
Aditya Patel <patel.aditya2014@gmail.com>
2024-10-08 23:12:39 +0530

    Time: 3 ms (66.29%), Space: 2.7 MB (13.84%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.go
@@ -0,0 +1,30 @@
+func searchMatrix(matrix [][]int, target int) bool {
+    // the solution is to map the linear index to matrix index
+    n := len(matrix)
+    m := len(matrix[0])
+    
+    l := 0
+    r := n * m
+    
+    for l <= r {
+        mid := (l + r ) / 2
+        
+        // get row and col index
+        rInd := mid / m
+        cInd := mid % m
+        
+        if rInd >= n || cInd >= m {
+            return false
+        }
+        
+        if matrix[rInd][cInd] == target {
+            return true
+        } else if matrix[rInd][cInd] < target {
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+    
+    return false
+}
