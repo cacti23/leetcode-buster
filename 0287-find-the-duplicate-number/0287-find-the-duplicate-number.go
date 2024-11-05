dfc4896f7a119ee84eecca0c0cbb289e91075732
Aditya Patel <patel.aditya2014@gmail.com>
2024-11-06 00:02:43 +0530

    Time: 6 ms (63.57%), Space: 9.4 MB (51.28%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
@@ -1,32 +1,27 @@
 func findDuplicate(nums []int) int {
-    // bitwise solution
-    // images 32 bits we will iterate over each bit
-    n := len(nums)
-    res := 0
+    slow, fast := 0, 0
     
-    for b := 0; b < 32; b++ {
-        x, y := 0, 0
-        mask := 1 << b
+    // find the loop
+    // set the value of slow
+    for {
+        slow = nums[slow]
+        fast = nums[nums[fast]]
         
-        // count the set bit at this bit position in array
-        for _, num := range nums {
-            if num & mask != 0 {
-                x++
-            }
-        }
-        
-        // count the set bit at this bit position in 1 to n range 
-        for i := 1; i < n; i++ {
-            if i & mask != 0 {
-                y++
-            }
-        }
-        
-        if x > y {
-            // we are doing this operation cuz imagine if the number is 2 then we can return res but if the number is 15 then we need to add mask to res 
-            res |= mask
+        if slow == fast {
+            break
         }
     }
     
-    return res
+    // as slow and fast method they will coincide if you start from zero
+    slow2 := 0
+    for {
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        
+        if slow == slow2 {
+            return slow
+        }
+    }
+    
+    return -1
 }
