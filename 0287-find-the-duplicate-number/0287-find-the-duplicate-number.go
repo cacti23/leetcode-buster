14e6cff41f0f0886dece8a6009644efd7195c08b
Aditya Patel <patel.aditya2014@gmail.com>
2024-11-05 23:47:55 +0530

    Time: 16 ms (37.86%), Space: 9.3 MB (51.28%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
@@ -1,23 +1,32 @@
 func findDuplicate(nums []int) int {
-    for _, num := range nums {
-        // map nums from 1 to n range to 0 to n - 1
-        idx := abs(num) - 1
-        // if it is already marked negative then that is the number 
-        if nums[idx] < 0 {
-            // this index value is negative means it was already marked negative
-            return abs(num)
-        } 
-
-        nums[idx] *= -1
-    }
-
-    return -1
-}
-
-func abs(num int) int {
-    if num < 0 {
-        return -num
+    // bitwise solution
+    // images 32 bits we will iterate over each bit
+    n := len(nums)
+    res := 0
+    
+    for b := 0; b < 32; b++ {
+        x, y := 0, 0
+        mask := 1 << b
+        
+        // count the set bit at this bit position in array
+        for _, num := range nums {
+            if num & mask != 0 {
+                x++
+            }
+        }
+        
+        // count the set bit at this bit position in 1 to n range 
+        for i := 1; i < n; i++ {
+            if i & mask != 0 {
+                y++
+            }
+        }
+        
+        if x > y {
+            // we are doing this operation cuz imagine if the number is 2 then we can return res but if the number is 15 then we need to add mask to res 
+            res |= mask
+        }
     }
     
-    return num
+    return res
 }
