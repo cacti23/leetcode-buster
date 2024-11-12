ff44e95d7ef5191b2671cb139a41b8bbc35640ff
Aditya Patel <patel.aditya2014@gmail.com>
2024-11-12 22:23:42 +0530

    Time: 88 ms (56.33%), Space: 76.7 MB (56.73%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.go
+++ 0146-lru-cache/0146-lru-cache.go
@@ -1,56 +1,83 @@
+// since struct are pass by value hence we need to use pointer
+type Node struct {
+    key, val int
+    prev, next *Node
+}
+
+// left and right will be treat as two extreme ends 
+// left next will always be least recently used
+// right prev will always be most recently used
 type LRUCache struct {
-    cache [][2]int
-    capacity int
+    cap         int
+    cache       map[int]*Node
+    left, right *Node
 }
 
 
 func Constructor(capacity int) LRUCache {
-    return LRUCache{
-        cache: make([][2]int, 0),
-        capacity: capacity,
-    }
-}
-
-
-func (this *LRUCache) Get(key int) int {
-    // Retrieve the value, remove it from its current position, and add it to the end of the array.
-    for i := range this.cache {
-        if this.cache[i][0] == key {
-            // get the array value in temp
-            tmp := this.cache[i]
-            // combine the array such that i index value is not present
-            this.cache = append(this.cache[:i], this.cache[i + 1:]...)
-            // now add the value at the end to follow the LRU principle
-            this.cache = append(this.cache, tmp)
-            return tmp[1]
-        }
-    }
-    // if the key is not present
-    return -1
-}
-
-
-func (this *LRUCache) Put(key int, value int)  {
-    // Check if thhe key present already update with the given value
-    for i := range this.cache {
-        if this.cache[i][0] == key {
-            // get the array value in temp
-            tmp := this.cache[i]
-            this.cache = append(this.cache[:i], this.cache[i+1:]...)
-            tmp[1] = value
-            // now add the value at the end to follow the LRU principle
-            this.cache = append(this.cache, tmp)
-            return
-        }
+    lru := LRUCache{
+        cache: make(map[int]*Node),
+        cap: capacity,
+        left: &Node{},
+        right: &Node{},
     }
     
-    // if the len is equal to cap then remove the first array
-    if len(this.cache) == this.capacity {
-        this.cache = this.cache[1:]
+    lru.left.next = lru.right
+    lru.right.prev = lru.left
+    return lru
+}
+
+func (lru *LRUCache) remove(node *Node) {
+    prev, next := node.prev, node.next
+    prev.next = next
+    next.prev = prev
+}
+
+func (lru *LRUCache) insert(node *Node) {
+    prev, next := lru.right.prev, lru.right
+    // insert between prev and next
+    prev.next = node
+    next.prev = node
+    node.next = next
+    node.prev = prev
+}
+
+
+func (lru *LRUCache) Get(key int) int {
+    cache := lru.cache
+    if node, ok := cache[key]; ok {
+        // update location make it most recent
+        lru.remove(node)
+        lru.insert(node)
+        return node.val
+    } 
+    
+    return -1 
+}
+
+
+func (lru *LRUCache) Put(key int, val int)  {
+    left := lru.left
+    cache := lru.cache
+    cap := lru.cap
+    // check if the element already exists
+    if node, ok := cache[key]; ok {
+        // remove this element
+        delete(cache, key)
+        lru.remove(node)
     }
     
-    this.cache = append(this.cache, [2]int{key, value})
-}
+    node := &Node{key: key, val: val}
+    cache[key] = node
+    lru.insert(node)
+    
+    if len(cache) > cap {
+        // remove least recently used
+        delete(cache, left.next.key)
+        lru.remove(left.next)
+
+    }
+} 
 
 
 /**
