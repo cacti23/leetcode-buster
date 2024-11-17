0171d08c7f2e7a79d689811be7fd8b9ce4d82c77
Aditya Patel <patel.aditya2014@gmail.com>
2024-11-17 16:34:17 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (16.98%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.go
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.go
@@ -7,15 +7,34 @@
  * }
  */
 func maxDepth(root *TreeNode) int {
-    // dfs recursive
+    // bfs iterative
     if root == nil {
         return 0
     }
     
-    left := maxDepth(root.Left)
-    right := maxDepth(root.Right)
+    queue := []*TreeNode{root}
+    maxDepth := 0
     
-    return 1 + max(left, right)
+    for len(queue) > 0 {
+        size := len(queue)
+
+        for i := 0; i < size; i++ {
+            curr := queue[0]
+            queue = queue[1:]
+            
+            if curr.Left != nil {
+                queue = append(queue, curr.Left)
+            }
+            
+            if curr.Right != nil {
+                queue = append(queue, curr.Right)
+            }
+        }
+        
+        maxDepth++
+    }
+    
+    return maxDepth
 }
 
 // tc -> O(n), n is number of total nodes
